7b29fca464acfcab1e889dedbc12de6dd594e0b7
EverettZ <everett@zettersten.com>
2019-07-23 14:26:33 -0400

    url mod

--- src/app/shared/models/urls.ts
+++ src/app/shared/models/urls.ts
@@ -1,1 +1,1 @@
-export const BASE_GIST = 'https://gist.githubusercontent.com/EverettZ/f2b2a4d15fced25bacb8a55556726b59/raw';
+export const BASE_GIST = 'https://gist.githubusercontent.com/EverettZ/f2b2a4d15fced25bacb8a55556726b59/raw/0dadb55101b440567353775cc89c79c1a20e85f7/resume.json';
