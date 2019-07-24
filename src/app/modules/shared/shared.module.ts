d273f576ede6671342355029daee38705f5ff7ac
EverettZ <everett@zettersten.com>
2019-07-24 07:55:21 -0400

    layouts shared module

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,3 +1,4 @@
+import { LayoutsModule } from './layouts/layouts.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -7,6 +8,9 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    LayoutsModule
   ]
 })
 export class SharedModule { }
